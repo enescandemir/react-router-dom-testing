9b43724147ca0e46cd08f905eef35b37d3fb4570
Bilal Enes Candemir <enescandemir26@gmail.com>
2023-03-20 22:58:40 +0300

    Moved BrowserRouter tag.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import AboutMe from './Pages/AboutMe';
 import Contact from './Pages/Contact';
 
 const AppRouter = () => (
-  <Router>
+  <div>
     <Link to="/">Home<br /></Link>
     <Link to="/aboutme">About Me<br /></Link>
     <Link to="/contact">Contact<br /></Link>
@@ -14,7 +14,7 @@ const AppRouter = () => (
       <Route path="/aboutme" element={<AboutMe />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
-  </Router>
+  </div>
 );
 
 export default AppRouter;
