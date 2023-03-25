a6ec7b5aa2c1f77806ec54ea4bdaf8fb63c6977a
Bilal Enes Candemir <enescandemir26@gmail.com>
2023-03-25 17:00:52 +0300

    Added counter to home page.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
 function Home() {
+    const [count, setCount] = useState(0);
+
     return <div>
         <h2>Home Page</h2>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
 }
 
